2fa0a0fac811959022571aaccaa890faeaf5edc9
Som Akudu <somakudu@gmail.com>
2025-04-27 17:30:01 +0100

    add board function to render cells and add cell component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,3 +1,22 @@
+import { BoardShape } from "../types";
+import Cell from "./Cell";
+
 interface Props {
   currentBoard: BoardShape;
 }
+
+function Board({currentBoard}: Props) {
+  return (
+    <div className="board">
+      {currentBoard.map((row, rowIndex) => (
+        <div className="row" key={`${rowIndex}`}>
+          {row.map((cell, colIndex) => (
+            <Cell key={`${rowIndex}-${colIndex}`} type={cell}/>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Board;
