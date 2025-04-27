e64e301a616af529a3d5f5c91bbe6b05e7c01217
Som Akudu <somakudu@gmail.com>
2025-04-27 17:46:44 +0100

    add board component to app, title and board styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Board from './components/Board'
+import { EmptyCell } from './types'
+
+const board = Array(20)
+.fill(null)
+.map(() => Array(12).fill(EmptyCell.Empty))
 
 function App() {
 
   return (
     <div className="App">
-
+      <h1 className="title"><strong>TETRIS</strong></h1>
+      <Board currentBoard={board}/>
     </div>
   )
 }
